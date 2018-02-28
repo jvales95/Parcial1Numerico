64c0c529ee24d1cf076c2bc4f9eb65dcf6a5734f
jvales95 <36300386+jvales95@users.noreply.github.com>
2018-02-28 08:01:14 -0500

    Update punto3a.r

--- punto3a.r
+++ punto3a.r
@@ -2,7 +2,7 @@ rm (list=ls())
 
 Fx <- function(x) log(x+2)+sin(x)
 #Se tiene f= ln(x+2) + sin x, ya que es la interseccion de las dos funciones solicitados
-F1x <- function(x) log(x+2)+sin(x) 
+F1x <- function(x) sin(x) 
 #Metodo de la Secante
 #Halla la raiz de Fx
 
@@ -10,7 +10,7 @@ error <- 1E-7
    
 secante <- function(x0,x1) {
     x<-(Fx(x1)*x0-Fx(x0)*x1)/(Fx(x1)-Fx(x0))
-    error <- 1
+    error <- 1E7
     while (error > 1.e-4) {
         x0<-x1
         x1<-x
